7b802f8427e9d6477991cd18500db6580c245375
luca <lucadoenges5@gmail.com>
2026-02-20 16:46:13 +0100

    Aufgabe 5 b

--- lab_3/source/gpu_LIN_upscaling.cu
+++ lab_3/source/gpu_LIN_upscaling.cu
@@ -13,7 +13,49 @@ std::uint32_t get_LIN_upscaled_height(std::uint32_t image_height){
     return (image_height * 2) -1;
 }
 
+__global__ void lin_upscale_kernel(const double* source_image, double* result, std::uint32_t* source_width, std::uint32_t* result_width, std::uint32_t* result_height) {
+    std::uint32_t x = blockIdx.x * blockDim.x + threadIdx.x;
+    std::uint32_t y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x < result_width && y < result_height) {
+        std::uint32_t inx = x/2;
+        std::uint32_t iny = y/2;
+        if (x%2 == 0 && y%2 == 0) {
+            result[y*result_width + x] = source_image[iny*source_width + inx];
+        }
+        else if (y%2 != 0 && x%2 == 0) {
+            double wert1 = source_image[iny*source_width + inx];
+            double wert2 = source_image[iny*source_width + inx +1];
+            result[y*result_width + x] = (wert1 + wert2) /2.0;
+        }
+        else if (x % 2 == 0 && y % 2 != 0) {
+            double wert1 = source_image[iny*source_width + inx];
+            double wert3 = source_image[(iny+1)*source_width + inx];
+            result[y*result_width + x] = (wert1 + wert3) / 2.0;
 
+        }
+        else {
+            double wert1 = source_image[iny*source_width + inx];
+            double wert2 = source_image[iny*source_width + inx +1];
+            double wert3 = source_image[(iny+1)*source_width + inx];
+            double wert4 = source_image[(iny+1)*source_width + inx +1];
+        }
+    }
+}
 
 void LIN_image_upscaling(void** d_source_image, std::uint32_t source_image_height, std::uint32_t source_image_width, void** d_result){
+    std::uint32_t width = get_LIN_upscaled_width(source_image_width);
+    std::uint32_t height = get_LIN_upscaled_height(source_image_height);
+
+    std::size_t size_ende = width*height * sizeof(double);
+    cudaMalloc(d_result, size_ende);
+
+    auto source_image = static_cast<const double *>(*d_source_image);
+    double* result = static_cast<double *>(d_result);
+
+    dim3 block_dim(16, 16);
+    dim3 grid_dim((width + block_dim.x -1) / block_dim.x, (height+ block_dim.y -1) / block_dim.y);
+
+    lin_upscale_kernel<<<grid_dim, block_dim>>>(source_image, result, source_image_width, source_image_height);
+
+    cudaDeviceSynchronize();
 }
--- lab_3/source/gpu_NN_upscaling.cu
+++ lab_3/source/gpu_NN_upscaling.cu
@@ -3,7 +3,6 @@
 #include <cstdio>
 #include <iostream>
 
-__global__ void nn_upscale_kernel(double* source, double dist, uint32_t )
 
 std::uint32_t get_NN_upscaled_width(std::uint32_t image_width){
     return image_width*3;
@@ -13,31 +12,32 @@ std::uint32_t get_NN_upscaled_height(std::uint32_t image_height){
     return image_height*3;
 }
 
-__global__ void nn_upscale_kernel(double* source, double resul, double length, uint32_t width, uint32_t height) {
-    uint32_t x = blockIdx.x * blockDim.x + threadIdx.x;
-    uint32_t y = blockIdx.y * blockDim.y + threadIdx.y;
+__global__ void nn_upscale_kernel(double* source, double resul, double length, std::uint32_t width, std::uint32_t height) {
+    std::uint32_t x = blockIdx.x * blockDim.x + threadIdx.x;
+    std::uint32_t y = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (x < width && y < height) {
-        uint32_t inx = x / 3;
-        uint32_t iny = y / 3;
+        std::uint32_t inx = x / 3;
+        std::uint32_t iny = y / 3;
     }
 
     resul[y * width + x] = source[iny * width + inx];
 }
 
 void NN_image_upscaling(void** d_source_image, std::uint32_t source_image_height, std::uint32_t source_image_width, void** d_result){
-    uint32_t width = get_NN_upscaled_width(source_image_width);
-    uint32_t height = get_NN_upscaled_height(source_image_height);
-    size_t size_ende = width*height * sizeof(double);
+    std::uint32_t width = get_NN_upscaled_width(source_image_width);
+    std::uint32_t height = get_NN_upscaled_height(source_image_height);
+    std::size_t size_ende = width*height * sizeof(double);
 
     cudaMalloc(d_result, size_ende);
 
-    auto source = reinterpret_cast<const double *>(d_source_image);
-    double* result = static_cast<double*>(cudaMalloc(d_result));
+    double *source = reinterpret_cast<const double *>(d_source_image);
+
+    auto* result = static_cast<double*>(cudaMalloc(d_result));
 
     dim3 block_dim(16, 16);
     dim3 grid_dim((width+ block_dim.x-1) / block_dim.x, (height + block_dim.y-1) / block_dim.y);
 
-    nn_upscale_kernel<<<gridDim, block_dim>>>(source, result, source_image_width, width, height);
+    nn_upscale_kernel<<<gridDim, block_dim>>>(source, *result, source_image_width, width, height);
     cudaDeviceSynchronize();
 }
