c4eae5a01427f297e46432e6496780979fa32b33
luca <lucadoenges5@gmail.com>
2026-02-22 19:41:49 +0100

    fehler

--- lab_3/source/gpu_reductions.h
+++ lab_3/source/gpu_reductions.h
@@ -2,4 +2,4 @@
 #include "intermediate_image.h"
 #include "common.cuh"
 
-double get_max_value(float* d_source_image, std::uint32_t total_pixels);
+double get_max_value(void** d_source_image, std::uint32_t source_image_height, std::uint32_t source_image_width);
