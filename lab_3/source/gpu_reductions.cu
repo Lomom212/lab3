e2115ffcb13b547c3861485b57b9a07a59dc552b
John <johnleon.fai@icloud.com>
2026-02-22 17:07:40 +0100

    aufgabe 6a, 7a, 7b, und 8

--- lab_3/source/gpu_reductions.cu
+++ lab_3/source/gpu_reductions.cu
@@ -4,30 +4,28 @@
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 
-__global__ void max_kernel(const float* image, std::uint32_t width, std::uint32_t height, float* d_max_val) {
-    std::uint32_t x = blockIdx.x * blockDim.x + threadIdx.x;
-    std::uint32_t y = blockIdx.y * blockDim.y + threadIdx.y;
+__global__ void max_kernel(const float* image, std::uint32_t total_pixels, float* d_max_val) {
+    std::uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (x < width && y < height) {
-        float pixel = image[y * width + x];
+    if (idx < total_pixels) {
+        float pixel = image[idx];
         atomicMax((int*)d_max_val, __float_as_int(pixel));
     }
 }
 
-double get_max_value(float* d_source_image, std::uint32_t total_pixels){
+double get_max_value(float* d_source_image, std::uint32_t total_pixels) {
     float h_max_result = 0.0f;
     float *d_max_result;
-    float *d_img = (float*)(*d_source_image);
 
     cudaMalloc(&d_max_result, sizeof(float));
-    cudaMemset(d_max_result,0,sizeof(float));
+    cudaMemset(d_max_result, 0, sizeof(float));
 
-    int  threadsPerBlock = 256;
-    int blocksperGrid((total_pixels + threadsPerBlock - 1) / threadsPerBlock);
+    int threadsPerBlock = 256;
+    int blocksperGrid = (total_pixels + threadsPerBlock - 1) / threadsPerBlock;
 
+    max_kernel<<<blocksperGrid, threadsPerBlock>>>(d_source_image, total_pixels, d_max_result);
 
-    max_kernel<<<blocksperGrid, threadsPerBlock>>>(d_img, source_image_width, source_image_height, d_max_result);
-    cudaMemcpy(&h_max_result,d_max_result, sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&h_max_result, d_max_result, sizeof(float), cudaMemcpyDeviceToHost);
     cudaFree(d_max_result);
 
     float final_result = *reinterpret_cast<float*>(&h_max_result);
